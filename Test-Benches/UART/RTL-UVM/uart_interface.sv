f6759aa1e2ecd02fbb597dfe58dec45dbe723015
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-04-28 12:06:19 +0530

    Update uart_interface.sv

--- Test-Benches/UART/RTL-UVM/uart_interface.sv
+++ Test-Benches/UART/RTL-UVM/uart_interface.sv
@@ -1,1 +1,17 @@
+interface uart_itf (input logic clk);
+  logic rst_n;
+  logic irq;
+  
+  //--------- Registers Interface -----------//
+  logic [4:0] addr;
+  logic write_en;
+  logic read_en;
+  logic [7:0] wdata;
+  logic [7:0] rdata;
 
+  //----------- UART Pins ------------//
+  logic tx;
+  logic rx;
+
+  clocking cb @(posedge
+endinterface
