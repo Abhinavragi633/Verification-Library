420dd3e2952df7f8887aed146a656b722dbc77b3
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-04-28 17:15:18 +0530

    Create uart_tb.sv

---
+++ Test-Benches/UART/RTL-UVM/uart_tb.sv
@@ -0,0 +1,30 @@
+module tb_top;
+  import uvm_pkg::*;
+  
+  bit clk;
+  always #10 clk <= !clk;
+
+  uart_itf itf_0 (clk);
+  uart_top dut_0 ( .clk(clk),
+                  .rst_n(itf_0.rst_n),
+                  .irq(itf_0.irq),
+                  .addr(itf_0.addr),
+                  .write_en(itf_0.write_en),
+                  .read_en(itf_0.read.en),
+                  .wdata(itf_0.wdata),
+                  .rdata(itf_0.rdata),
+                  .tx(itf_0.tx),
+                  .rx(itf_0.rx) );
+  // At start of simulation, set the interface handle as a config object in UVM database. 
+  // This IF handle can be retrieved in the test using the get() method run_test () accepts the test name as argument. 
+  // In this case, base_test will be run for simulation.
+  initial begin
+    uvm_config_db #(virtual uart_itf)::set (null, "uvm_test_top", "uart_itf", itf_0);
+    run_test("reg_test");
+  end
+
+  initial begin
+    $dumpvars;
+    $dumfile("dump.vcd");           
+  end
+endmodule
