0d72bbda61af6cc70d327a1d227bc9e1cb2af34f
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-04-28 14:54:44 +0530

    Create uart_monitor.sv

---
+++ Test-Benches/UART/RTL-UVM/uart_monitor.sv
@@ -0,0 +1,30 @@
+class montr extends uvm_monitor;
+  `uvm_component_utils(montr)
+
+  function new(string name = "montr", uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+  uvm_analysis_port #(seq_item) montr_ana_port;
+  virtual uart_itf v_itf;
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if (!uvm_config_db#(virtual uart_itf)::get(this, "", "uart_itf", v_itf)) begin
+      `uvm_fatal("MONITOR", "Could not find the virtual interface v_itf")
+    end
+    montr_ana_port = new("mon_ana_port", this);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.new(phase);
+
+    forever begin
+      @(v_itf_cb);
+      if (v_itf.rstn) begin
+        seq_item sqn_itm = seq_item::type_id::create("sqn_itm");
+        sqn_itm.addr = v_itf.addr;
+      end
+    end
+  endtask
+endclass
