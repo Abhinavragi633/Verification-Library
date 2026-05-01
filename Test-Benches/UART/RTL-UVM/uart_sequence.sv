f60d8b227accecc04cf9e41d495dd72235db0f79
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-01 23:22:42 +0530

    Rename seqnc to uart_seq and update item type

--- Test-Benches/UART/RTL-UVM/uart_sequence.sv
+++ Test-Benches/UART/RTL-UVM/uart_sequence.sv
@@ -1,7 +1,7 @@
-class seqnc extends uvm_sequence #(seq_item);   // Without Parameterization Driver/Sequence Hand Shake Breaks.
-  `uvm_object_utils(seqnc)
+class uart_seq extends uvm_sequence #(uart_seq_item);   // Without Parameterization Driver/Sequence Hand Shake Breaks.
+  `uvm_object_utils(uart_seq)
 
-  function new(string name = "seqnc");
+  function new(string name = "uart_seq");
     super.new(name);
   endfunction
 
@@ -9,7 +9,7 @@ class seqnc extends uvm_sequence #(seq_item);   // Without Parameterization Driv
 
   virtual task body();
     for (int i = 0; i < num; i++) begin
-      seq_item sqn_itm = seq_item::type_id::create("sqn_itm_%0d",i);
+      uart_seq_item sqn_itm = uart_seq_item::type_id::create("sqn_itm_%0d",i);
       start_item(sqn_itm);
       sqn_itm.randomize();
       if (!sqn_itm.randomize()) `uvm_error("SEQUENCE", "Randomization failed")
