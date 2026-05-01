a522f5330e56f9848f48f4b3de0f735268612a90
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-01 15:55:31 +0530

    Update uart_clk_driver.sv
    changes to clock generation

--- Test-Benches/UART/RTL-UVM/uart_clk_driver.sv
+++ Test-Benches/UART/RTL-UVM/uart_clk_driver.sv
@@ -5,6 +5,7 @@ class clk_drv extends uvm_driver #(clk_seq_item);
 	
 	virtual function new(string name,uvm_component parent);
 		super.new(name,parent);
+		`uvm_info("CLK_DRV(new)",$sformatf("Instantiated new %s in parent %s",name,parent.get_name()))
 	endfunction
 
 	virtual function void build_phase(uvm_phase phase);
@@ -24,20 +25,24 @@ class clk_drv extends uvm_driver #(clk_seq_item);
 		`uvm_info("CLK_DRV(Run Phase)",$sformatf("clk_v_itf.clk is Initialized to %0b", clk_v_itf.clk))
 
 		forever begin
-			`uvm_info("CLK_DRV(Run Phase)",$sformatf("Requesting sequence from sequencer........", clk_v_itf.clk))
+			`uvm_info("CLK_DRV(Run Phase)",$sformatf("Requesting clock sequence item from sequencer........"))
 			seq_item_port.get_next_item(item_frm_seqr);
-			`uvm_info("CLK_DRV(Run Phase)",$sformatf("", clk_v_itf.clk))
+			`uvm_info("CLK_DRV(Run Phase)",$sformatf("Received clock sequence item from sequencer."))
+			`uvm_info("CLK_DRV(Run Phase)",$sformatf("item_frm_seqr.clk_en = %0b item_frm_seqr.clk_freq = %0f MHz",item_frm_seqr.clk_en,item_frm_seqr.clk_freq))
 
 			if(!item_frm_seqr.clk_en) begin
 				clk_v_itf.clk <= 'z;
+				`uvm_info("CLK_DRV(Run Phase)","item_frm_seqr.clk_en is not HIGH. So, Driving Hi-Z into clock interface.")
+				`uvm_info("CLK_DRV(Run Phase)","item_done is invoked for handshake with sequencer.")
 				seq_item_port.item_done();
+				`uvm_info("CLK_DRV(Run Phase)","item_done is returned for handshake with sequencer.")
 				continue;
 			end
 
-			half_period = (1000.0/item_frm_seqr)/2.0;
+			half_period = (1000.0/item_frm_seqr.clk_freq)/2.0;
 			seq_item_port.item_done();
 			
-			while (item_frm_seqr.clk_en) begin
+			while (!seq_item_port.has_do_available()) begin
 				clk_v_itf.clk <= 0;
 				#(half_period);
 				clk_v_itf.clk <= 1;
