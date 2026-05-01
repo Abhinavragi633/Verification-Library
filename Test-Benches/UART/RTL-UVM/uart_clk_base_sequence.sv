747ca01cbbd8583010444d0beaa427ecb11d223e
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-01 13:28:49 +0530

    Update uart_clk_base_sequence.sv

--- Test-Benches/UART/RTL-UVM/uart_clk_base_sequence.sv
+++ Test-Benches/UART/RTL-UVM/uart_clk_base_sequence.sv
@@ -6,8 +6,8 @@ class base_clk_seq extends uvm_sequence #(clk_seq_item);
 		clk_seq_item clk_seq0;
 		
 		clk_seq0 = clk_seq_item::type_id::create("clk_seq0");
-		clk_cfg0.clk_freq = 100;	//  Clock Frequency Set to 100 MHz.
-		clk_cfg0.clk_en = 0;
+		clk_seq0.clk_freq = 100;	//  Clock Frequency Set to 100 MHz.
+		clk_seq0.clk_en = 0;
 		start_item(clk_seq0);
 		finish_item(clk_seq0);
 		
