df562952dfca6320aa6773b1f4d1a8dcdb9f2619
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-01 11:32:45 +0530

    Create uart_clk_cfg_object.sv

---
+++ Test-Benches/UART/RTL-UVM/uart_clk_cfg_object.sv
@@ -0,0 +1,11 @@
+// uvm_object declaration for clk_cfg_obj for clk_agent
+class clk_cfg_obj extends uvm_object;
+  `uvm_object_utils(clk_cfg_obj)
+  
+  real clk_freq;
+  bit clk_en;
+  
+  virtual function new (string name = "clk_cfg_obj", parent = null);
+    super.new(name,parent);
+  endfunction
+endclass
