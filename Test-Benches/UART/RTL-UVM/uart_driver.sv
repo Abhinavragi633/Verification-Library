efb84fb4a90ec21cb01a6ae0ceecb00d26578586
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-01 23:33:00 +0530

    Refactor uart_driver to uart_drvr with updated interfaces

--- Test-Benches/UART/RTL-UVM/uart_driver.sv
+++ Test-Benches/UART/RTL-UVM/uart_driver.sv
@@ -1,23 +1,27 @@
-class drvr extends uvm_driver #(seq_item);
-  `uvm_component_utils(drvr)
+class uart_drvr extends uvm_driver #(uart_seq_item);
+  `uvm_component_utils(uart_drvr)
 
-  function new(string name = "drvr", uvm_component parent=null);
+  function new(string name = "uart_drvr", uvm_component parent=null);
     super.new(name,parent);
   endfunction
 
-  virtual uart_itf v_itf;
+  virtual uart_interface uart_v_itf;
+  virtual clk_interface clk_v_itf;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if (!uvm_config_db#(virtual uart_itf)::get(this, "", "uart_itf", v_itf)) begin
-      `uvm_fatal("DRIVER", "Could not find virtual interface.")
+    if (!uvm_config_db#(virtual uart_itf)::get(this, "", "uart_v_itf", uart_v_itf)) begin
+      `uvm_fatal("DRIVER", "Could not find virtual uart interface.")
+    end
+    if (!uvm_config_db#(virtual clk_interface)::get(this, "", "clk_v_itf", clk_v_itf)) begin
+      `uvm_fatal("DRIVER", "Could not find virtual clock interface.")
     end
   endfunction
 
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
     forever begin
-      seq_item sqn_itm;
+      uart_seq_item sqn_itm;
       `uvm_info("DRIVER", $sformatf("Waiting for item from Sequencer"), UVM_HIGH)
       seq_item_port.get_next_item(sqn_itm);
       drive_item(sqn_itm);
@@ -26,12 +30,11 @@ class drvr extends uvm_driver #(seq_item);
   endtask
 
   virtual task drive_item(seq_item sqn_itm);
-    @(v_itf.cb);
-    v_itf.cb.addr <= sqn_itm.addr;
-    v_itf.cb.write_en <= sqn_itm.write_en;
-    v_itf.cb.read_en <= sqn_itm.read_en;
-    v_itf.cb.wdata <= sqn_itm.wdata;
-    v_itf.cb.rx <= sqn_itm.rx;
-    v_itf.rst_n <= sqn_itm.rst_n;
+    @(posedge clk_v_itf.clk);
+    uart_v_itf.cb.addr <= sqn_itm.addr;
+    uart_v_itf.cb.write_en <= sqn_itm.write_en;
+    uart_v_itf.cb.read_en <= sqn_itm.read_en;
+    uart_v_itf.cb.wdata <= sqn_itm.wdata;
+    uart_v_itf.cb.rx <= sqn_itm.rx;
   endtask
 endclass
