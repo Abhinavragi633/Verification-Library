8f42e60a5bf47bb373dd1d7adb6c2cf8af4a6538
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-18 16:08:44 +0530

    Update uart_clk_driver.sv

--- Test-Benches/UART/RTL-UVM/uart_clk_driver.sv
+++ Test-Benches/UART/RTL-UVM/uart_clk_driver.sv
@@ -1,45 +1,52 @@
+// Class Declaration of clk Driver.
 class clk_drv extends uvm_driver #(clk_seq_item);
+	// For UVM Factory Registration.
 	`uvm_component_utils(clk_drv)
 
-	virtual clk_interface clk_v_itf;
+	virtual uart_itf vitf;  // Declared as virtual as classes cannot access HDL Constructs directly.
 	
 	virtual function new(string name,uvm_component parent);
 		super.new(name,parent);
-		`uvm_info("CLK_DRV(new)",$sformatf("Instantiated new %s in parent %s",name,parent.get_name()))
+		`uvm_info("CLK_DRV -> new()",$sformatf("Constructed a new clk_drv with name %s in parent %s",name,parent.get_name()), UVM_HIGH)
 	endfunction
 
 	virtual function void build_phase(uvm_phase phase);
+		`uvm_info("CLK_DRV -> build_phase()",$sformatf("Build Phase for %s has been started.",this.name),UVM_HIGH)
 		super.build_phase(phase);
-		if(!uvm_config_db#(virtual clk_interface)::get(this,"","clk_v_itf",clk_v_itf)) begin
-			`uvm_fatal("CLK_DRV(Build Phase)","Could not get virtual clock interface from UVM Config DB.")
+		
+		// Starting from this component, key is searched in upwards heirarchy. If found assigned to vitf.
+		if(!uvm_config_db#(virtual uart_itf)::get(this,"","uart_vitf",vitf)) begin
+			`uvm_fatal("CLK_DRV -> build_phase()","Could not get virtual clock interface from UVM Config DB.")
 		end else begin
-			`uvm_info("CLK_DRV(Build Phase)","Got virtual clock interface from UVM Config DB.")
+			`uvm_info("CLK_DRV -> build_phase()","Got virtual clock interface from UVM Config DB.", UVM_HIGH)
 		end
 	endfunction
 
 	virtual task run_phase(uvm_phase phase);
-		clk_seq_item item_frm_seqr;
+		`uvm_info("CLK_DRV -> run_phase()",$sformatf("Run Phase for %s has been started.",this.name),UVM_HIGH)
+		clk_seq_item clk_item_frm_seqr;
 		real half_period;
 
-		clk_v_itf.clk <= 'z;
-		`uvm_info("CLK_DRV(Run Phase)",$sformatf("clk_v_itf.clk is Initialized to %0b", clk_v_itf.clk))
+		//  Clk is initialized to Z.
+		vitf.clk <= 'z;
+		`uvm_info("CLK_DRV(Run Phase)",$sformatf("clk_v_itf.clk is Initialized to %0b", clk_v_itf.clk),UVM_LOW)
 
 		forever begin
-			`uvm_info("CLK_DRV(Run Phase)",$sformatf("Requesting clock sequence item from sequencer........"))
-			seq_item_port.get_next_item(item_frm_seqr);
-			`uvm_info("CLK_DRV(Run Phase)",$sformatf("Received clock sequence item from sequencer."))
-			`uvm_info("CLK_DRV(Run Phase)",$sformatf("item_frm_seqr.clk_en = %0b item_frm_seqr.clk_freq = %0f MHz",item_frm_seqr.clk_en,item_frm_seqr.clk_freq))
+			`uvm_info("CLK_DRV(Run Phase)",$sformatf("Requesting clock sequence item from sequencer........"),UVM_MEDIUM)
+			seq_item_port.get_next_item(clk_item_frm_seqr);
+			`uvm_info("CLK_DRV(Run Phase)",$sformatf("Received clock sequence item from sequencer."),UVM_MEDIUM)
+			`uvm_info("CLK_DRV(Run Phase)",$sformatf("item_frm_seqr.clk_en = %0b item_frm_seqr.clk_freq = %0f MHz",clk_item_frm_seqr.clk_en,clk_item_frm_seqr.clk_freq),UVM_LOW)
 
-			if(!item_frm_seqr.clk_en) begin
-				clk_v_itf.clk <= 'z;
-				`uvm_info("CLK_DRV(Run Phase)","item_frm_seqr.clk_en is not HIGH. So, Driving Hi-Z into clock interface.")
+			if(!clk_item_frm_seqr.clk_en) begin
+				vitf.clk <= 'z;
+				`uvm_info("CLK_DRV(Run Phase)","clk_item_frm_seqr.clk_en is not HIGH. So, Driving Hi-Z into clock interface.")
 				`uvm_info("CLK_DRV(Run Phase)","item_done is invoked for handshake with sequencer.")
 				seq_item_port.item_done();
 				`uvm_info("CLK_DRV(Run Phase)","item_done is returned for handshake with sequencer.")
 				continue;
 			end
 
-			half_period = (1000.0/item_frm_seqr.clk_freq)/2.0;
+			half_period = (1000.0/clk_item_frm_seqr.clk_freq)/2.0;
 			seq_item_port.item_done();
 			
 			while (!seq_item_port.has_do_available()) begin
