cb812b488e04ee52b4fd74315090e193e2cd5267
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-21 12:40:42 +0530

    Update uart_clk_monitor.sv

--- Test-Benches/UART/RTL-UVM/uart_clk_monitor.sv
+++ Test-Benches/UART/RTL-UVM/uart_clk_monitor.sv
@@ -1,42 +1,61 @@
+// Class Declaration of Clock Object for Clk Monitor.
 class clk_obj extends uvm_sequence_item;
+    // Factory Registration
+    `uvm_object_utils_begin(clk_obj)
+        `uvm_field_int(clk, UVM_ALL_ON)
+    `uvm_object_utils_end
+    
     logic clk;
-    function new(string name = clk_obj);
-        super.new();
+    
+    function new(string name = "clk_obj");
+        super.new(name);
+        `uvm_info("CLK_OBJ -> new()",$sformatf("A new clk object is created by the name %s",name), UVM_HIGH)
     endfunction
 endclass
 
+// Class Declaration for Clcok Monitor to monitor clk signal of the interface.
 class clk_mntr extends uvm_monitor;
   `uvm_component_utils(clk_mntr)
 
   function new(string name = "clk_mntr", uvm_component parent=null);
     super.new(name,parent);
+      // Print can crash if parent_name returns a null value. So Null is handles using conditional string formatting.
+      `uvm_info("CLK_MNTR -> new()",$sformat("A new clock monitor is constructed in %s by %s",(parent != null) ? parent.get_name() : "null",name),UVM_HIGH)
   endfunction
 
-  uvm_analysis_port #(clk_obj) clk_montr_ana_port;
+    uvm_analysis_port #(clk_obj) clk_montr_ana_port;  // Declaration of a Analysis Port for Subscriber and Scoreboard.
   
-  virtual clk_interface clk_v_itf;
+  virtual uart_itf vitf;
 
   virtual function void build_phase(uvm_phase phase);
+      `uvm_info("CLK_MNTR -> build_phase()","Build Phase is Started.",UVM_HIGH)
     super.build_phase(phase);
     
-    if (!uvm_config_db#(virtual clk_interface)::get(this, "", "clk_v_itf", clk_v_itf)) begin
-      `uvm_fatal("MONITOR", "Could not find the virtual clock interface.")
+      if (!uvm_config_db#(virtual uart_itf)::get(this,"", "uart_vitf", vitf)) begin
+      `uvm_fatal("CLK_MNTR -> build_phase()", "Could not find the virtual clock interface.")
     end
-    clk_montr_ana_port = new("clk_montr_ana_port", this);
+      else begin
+          `uvm_info("CLK_MNTR -> build_phase()","Found the virtual interface from UVM Config DB.", UVM_MEDIUM)
+      end
+      `uvm_info("CLK_MNTR -> build_phase()","Constructing a New Analysis Port ...........", UVM_MEDIUM)
+      // clk_montr_ana_port = uvm_analysis_port#(clk_obj)::type_id::create("clk_montr_ana_port", this);   This is wrong because Analysis Port is not a factory object.
+      clk_montr_ana_port = new("clk_montr_ana_port", this);
   endfunction
 
   virtual task run_phase(uvm_phase phase);
+      `uvm_info("CLK_MNTR -> run_phase()","Run Phase has started.",UVM_HIGH)
     super.run_phase(phase);
 
     forever begin
+        `uvm_info("CLK_MNTR -> run_phase()","Constructing a New Clk object ...........", UVM_MEDIUM)
         clk_obj seqn_itm = clk_obj::type_id::create("seqn_itm");
         
-        @(clk_v_itf.clk)
-        seqn_itm.clk = clk_v_itf.clk;
+        @(vitf.clk);
+        seqn_itm.clk = vitf.clk;
         
         clk_montr_ana_port.write(seqn_itm);
-        
-        `uvm_info("MONITOR", $sformatf("Observed transaction:\n%s",seqn_itm.clk),UVM_HIGH)
+        `uvm_info("CLK_MNTR -> run_phase()","Observed transaction in Clock Montor is : ",UVM_LOW)
+        seqn_itm.print();
 
     end
   endtask
