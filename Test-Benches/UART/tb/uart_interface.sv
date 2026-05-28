ca1df7e34c2cbf0738fdc87460cdaf907ee11cf8
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-28 15:30:16 +0530

    Update uart_interface.sv

--- Test-Benches/UART/tb/uart_interface.sv
+++ Test-Benches/UART/tb/uart_interface.sv
@@ -1,1 +1,14 @@
-
+interface uart_itf ;  // SV doesn't allow empty port list. So don't use uart_itf ();
+	logic clk;
+	logic rst_n;
+	
+	logic tx;
+	logic rx;
+	logic irq;
+	
+	logic write_en;
+	logic read_en;
+	logic [4:0] addr;
+	logic [7:0] wdata;
+	logic [7:0] rdata;
+endinterface
