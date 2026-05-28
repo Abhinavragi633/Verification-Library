b5fac3c4e8d4158d099332712152c8c7be6dd18b
Abhinavragi633 <78337099+Abhinavragi633@users.noreply.github.com>
2026-05-28 15:23:51 +0530

    Delete Test-Benches/UART/RTL-UVM directory

--- Test-Benches/UART/RTL-UVM/uart_rst_sequence_item.sv
+++
@@ -1,8 +0,0 @@
-class rst_seq_item extends uvm_sequence_item;
-	`uvm_object_utils(rst_seq_item)
-	logic rst_n;
-	function new (string name = rst_seq_item);
-		super.new(name);
-		`uvm_info("RST_SEQ_ITEM(new)",$sformatf("Instantiated new %s created.",name))
-	endfunction
-endclass
